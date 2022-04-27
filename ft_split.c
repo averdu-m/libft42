7c3143941c15f3c5921dfd33aad2fbfe0c2820ce
Adrian Verdu martin <averdu-m@c2r13s5.42madrid.com>
2022-04-27 17:00:27 +0200

    parte obligatoria

--- ft_split.c
+++ ft_split.c
@@ -10,24 +10,85 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-static void count(char const *s, char c)
+#include "libft.h"
+
+static char	**ft_free(char **words)
 {
-	int i;
-	int num;
+	int	i;
 
 	i = 0;
-	num = 1;
-
-	while(s[i] != '\0')
+	while (words[i] != NULL)
 	{
-		if (i > 0 && s[i] == c)
-			num++;
+		free(words[i]);
 		i++;
 	}
-	return(num);
+	free(words);
+	return (NULL);
 }
 
-char **ft_split(char const *s, char c)
+static int	ft_count(char const *s, char c)
 {
+	int	i;
+	int	num;
 
+	i = 0;
+	num = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		while (s[i] && s[i] != c)
+		{
+			i++;
+		}
+		num++;
+	}
+	if (s[i - 1] == c)
+		num--;
+	if (num <= 0)
+		return (0);
+	return (num);
+}
+
+static char	**ft_initsplit(const char *s, char c)
+{
+	int		count;
+	char	**words;
+
+	if (!s)
+		return (NULL);
+	count = ft_count(s, c);
+	words = malloc(sizeof(char *) * (count + 1));
+	if (!words)
+		return (NULL);
+	words[count] = NULL;
+	return (words);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**words;
+	int		start;
+	int		wu;
+	int		i;
+
+	words = ft_initsplit(s, c);
+	if (!words)
+		return (NULL);
+	wu = 0;
+	i = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (!s[i])
+			break ;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		words[wu++] = ft_substr(s, start, i - start);
+		if (words[wu - 1] == NULL)
+			return (ft_free(words));
+	}
+	return (words);
 }
